c2cd30d13ca5f6be2a00dfad770dc0ed0e371e51
TheFeather293 <113765644+TheFeather293@users.noreply.github.com>
2026-05-16 21:22:30 +0300

    Add files via upload

---
+++ manifest.lua
@@ -0,0 +1,75 @@
+return {
+    house = {
+        house_pos = Vector3.new(-9000,4000,3000),
+        unique = "house_{aaf85136-00df-4dfa-a9ba-169852076329}",
+        active_addons = {
+        },
+        building_type = "queenslander_home",
+        house_id = 23,
+    },
+    textures = {
+        Room7 = {
+            floors = "white_brick",
+            walls = "stripes_white",
+        },
+        Room5 = {
+            floors = "wood_planks_beige",
+            walls = "stripes_white",
+        },
+        Room11 = {
+            floors = "white_brick",
+        },
+        Room4 = {
+            floors = "tile_white",
+            walls = "white_brick",
+        },
+        Room10 = {
+            floors = "wood_planks_white",
+            walls = "cream",
+        },
+        Room14 = {
+            floors = "tile_white",
+            walls = "white_brick",
+        },
+        Room3 = {
+            floors = "wood_planks_beige",
+            walls = "stripes_white",
+        },
+        Room9 = {
+            floors = "light_grey",
+            walls = "whiteleaf",
+        },
+        Room2 = {
+            floors = "wood_planks_beige",
+        },
+        Room13 = {
+            floors = "wood_planks_beige",
+            walls = "stripes_white",
+        },
+        Room6 = {
+            floors = "wood_planks_beige",
+            walls = "stripes_white",
+        },
+        Room1 = {
+            floors = "wood_planks_beige",
+            walls = "stripes_white",
+        },
+        Room8 = {
+            floors = "wood_planks_beige",
+            walls = "stripes_white",
+        },
+    },
+    metadata = {
+        totalFurniture = 3903,
+        originalOwner = "r3bzyyxq",
+        date = "2026-04-08 00:35:07",
+        savedBy = "SurgeArrow7078",
+        totalChunks = 2,
+        version = "2.0",
+        chunkSize = 2000,
+        chunked = true,
+        timestamp = 1775622907,
+        savedByUserId = 10671343269,
+        totalValue = 46341,
+    },
+}
